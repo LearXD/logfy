05bb24e5565fb8b2bfca54aa84c7cd9c0205e306
LearXD <gutyxdcanal@gmail.com>
2023-01-23 20:10:44 -0300

    Tests...

---
+++ index.js
@@ -0,0 +1,5 @@
+const cors = require('cors');
+
+module.exports = () => {
+    return cors.hex('#F00')('Hello World')
+}
---
+++ test/index.js
@@ -0,0 +1,4 @@
+const { Logfy } = require('../index')
+
+const Logger = new Logfy();
+Logger.teste("AAA")
